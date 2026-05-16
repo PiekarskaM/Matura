916cbbf343cd48c9497ff8fbfa81847961e92fbd
bakerm <mpiekarska@gmail.com>
2026-05-16 09:49:00 +0200

    Poprawka - matura maj 2026 zad. 1.2

--- Matura maj 2026/zad12.py
+++ Matura maj 2026/zad12.py
@@ -5,13 +5,14 @@ def A(m,n):
     elif n > 1 and n%2==0:
         return A(2*m,n//2)
     elif n > 1 and n%2==1:
-        return 2*A(m,(n-1)//2)
+        return 2*A(m,(n-1)//2)+m
 
 
 
 
 print(A(1,777))
 print(A(2*(10**6),256*(10**6)))
+print()
 
 
 
